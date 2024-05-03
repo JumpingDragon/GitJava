324ba67c9e38f7f4502a5f94dbb5f7dcd4e7b41c
JumpingDragon <bori555@ewhain.net>
2024-05-03 09:41:26 +0900

    MainClass 추가

---
+++ src/testProject_240503/MainClass.java
@@ -0,0 +1,10 @@
+package testProject_240503;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("We were both young when I first saw you");
+	}
+
+}
